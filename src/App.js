30a7e49f4f8adf7448b7b13d0803418c055e5f88
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-23 13:57:17 +0100

    fixed App.js: removed default setup and replaced it with codes for jimmy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+// import './css/bootstrap-theme.css';
+// import './css/bootstrap-theme.min.css';
+
+// import './css/bootstrap.min.css';
+import './css/templatemo-style.css';
+import './css/bootstrap.css';
+import { Home } from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-				<p>
-					<a
-						className="App-link"					
-          	href="https://github.com/DafetiteOgaga/custom_commands"					
-          	target="_blank"
-						rel="noopener noreferrer"
-					>
-          	Install more custom commands
-					</a>
-				</p>
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
