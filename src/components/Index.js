f7daf6675578cfc9ce3464ba6e5ab5b4e75b2eff
Dafetite Ogaga <133142029+DafetiteOgaga@users.noreply.github.com>
2025-06-17 18:49:38 +0100

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -7,7 +7,7 @@ import { Socials } from "./Socials"
 // import * as Ficons from "react-icons/fa";
 
 function Index () {
-	// console.log(remot4444
+	// console.log remote55555
 
 	// the sections display and scroll animation
 	useEffect(() => {
