04c6c381819ddfa8d41e37903754c09712cc0bc1
Dafetite Ogaga <133142029+DafetiteOgaga@users.noreply.github.com>
2025-06-17 17:17:29 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home () {
 	const isMobile = useIsMobile();
 	return (
 		<>
-			{/* home secremote11111 */}
+			{/* home secremote22222 */}
 			<section className="panel b-blue" id="1">
 				<article className="panel__wrapper">
 					<div className="panel__content">
