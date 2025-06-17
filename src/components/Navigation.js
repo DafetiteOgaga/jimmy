e1f94a5a3a4c93be83cf33409f9ea0d3f0d59e39
Dafetite Ogaga <133142029+DafetiteOgaga@users.noreply.github.com>
2025-06-17 19:01:00 +0100

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ function Navigation () {
 	const toggleMenuClickHandler = () => {setIsMenuClicked(!isMenuClicked)};
 	const hoverHandler = (index, value) => {
 		setOnHover(prev => {
-			// console.log('p22222323rev:', remote66666
+			// console.log('p22222323rev:', remote7777
 			const updated = [...prev];
 			updated[index] = value;
 			return updated;
