3e397fc458634cb0d80a5a0a84cf2c89275cb24b
Dafetite Ogaga <133142029+DafetiteOgaga@users.noreply.github.com>
2025-06-17 18:39:31 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact () {
 	const year = DateHook().todayYear;
 	return (
 		<>
-			{/* contact sectionremote33333 */}
+			{/* contact sectionremote4444 */}
 			<section className="panel b-green" id="4">
 				<article className="panel__wrapper">
 					<div className="panel__content">
