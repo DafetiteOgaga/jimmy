6d6a4612ac561217878cb781236c99726d907a0c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-06-18 01:13:17 +0100

    Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact () {
 	const year = DateHook().todayYear;
 	return (
 		<>
-			{/* contact local9876 */}
+			{/* contat local9876 */}
 			<section className="panel b-green" id="4">
 				<article className="panel__wrapper">
 					<div className="panel__content">
