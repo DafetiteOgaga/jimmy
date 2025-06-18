05e7c9e7625095d8b6dbc8a5336fa6ef1f53b986
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-06-18 01:24:44 +0100

    Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home () {
 	const isMobile = useIsMobile();
 	return (
 		<>
-			{/* home remote1234 */}
+			{/* hom remote1234 */}
 			<section className="panel b-blue" id="1">
 				<article className="panel__wrapper">
 					<div className="panel__content">
